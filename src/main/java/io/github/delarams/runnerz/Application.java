21f5d2b6fd7392d789426c750eab791c09bd3b4b
delaram <delaram.bahreiniesfa@ucalgary.ca>
2024-12-28 02:35:27 -0700

    In memory database added

--- src/main/java/io/github/delarams/runnerz/Application.java
+++ src/main/java/io/github/delarams/runnerz/Application.java
@@ -3,6 +3,7 @@ package io.github.delarams.runnerz;
 //import io.github.delarams.runnerz.WelcomeMessage;
 import io.github.delarams.runnerz.run.Location;
 import io.github.delarams.runnerz.run.Run;
+import io.github.delarams.runnerz.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -25,13 +26,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 
 	}
-	@Bean
-	CommandLineRunner runner(){
-		return args -> {
-			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR );
-			log.info("Run: "+ run);
-		};
+//	@Bean
+//	CommandLineRunner runner(RunRepository runRepository){
+//		return args -> {
+//			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR );
+//			runRepository.create(run);
+//		};
 	}
 
-
-}
---
+++ src/main/java/io/github/delarams/runnerz/run/Runs.java
@@ -0,0 +1,6 @@
+package io.github.delarams.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+}
